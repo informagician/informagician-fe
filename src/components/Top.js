ba0abce4c3574a4d3eba35e4ee70065857455073
Milo Rastgoo <msrastgoo@gmail.com>
2020-07-10 16:39:07 -0400

    styles modified, testing with react-hook-forms

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Logo from './Logo'
 import Nav from './Nav'
 
 const Top = () => {
 
+    const [ scroll, setScroll] = useState(false)
+
+    window.addEventListener('scroll',()=>{
+        
+        if(window.scrollTop > 0) {
+            setScroll(true)
+            console.log('TRUE')
+        }
+    })
+
+    // useEffect(() => {
+    //     if (document.body.scrollTop != 0){
+    //         setScroll(true)
+    //         console.log('TRUE')
+    //     }
+    // },[])
+    
+
     return(
-        <div className="top">
+        <div className="top" id={scroll ? 'scrolled' : null}>
             <div className="container">
                 <Logo />
                 <Nav />
