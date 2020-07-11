433d4733774867fc330f05faf27ea4ddc775c0f3
Milo Rastgoo <msrastgoo@gmail.com>
2020-07-11 10:42:34 -0400

    change of structure to 100vh

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,14 @@ const About = () => {
         <section className="about">
             <div className="container">
                 <h2>About Me</h2>
-                <img src="./assets/milo-rastgoo.png" />
+                <div className="content">
+                    <div className="text">
+                        <p>My name is Milo Rastgoo, I am a Full-Stack Web Developer with experiece as Network Specialist and IT Project Manager.</p>
+                        <p>I'm interested in solving problems and making things, either using code or collection of hardware tools!</p>
+                    </div>
+                    <img src="./assets/milo-rastgoo.png" />
+                </div>
             </div>
-        
         </section>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,15 @@ const Header = () => {
 
     return(
         <header>
-            <div className="jumbo">
-                <div>
-                    <img src="./assets/milo-rastgoo-avatar.png" />
-                </div>
-                <div className="jumbo-text">
-                    <h2>Hi! I'm Milo.</h2>
-                    <p>I'm a Full-Stack Web Developer.</p>
+            <div className="container">
+                <div className="jumbo">
+                    <div>
+                        <img src="./assets/milo-rastgoo-avatar.png" />
+                    </div>
+                    <div className="jumbo-text">
+                        <h2>Hi! I'm Milo.</h2>
+                        <p>I'm a Full-Stack Web Developer.</p>
+                    </div>
                 </div>
             </div>
         </header>
